f3ef8e248ec105daa18006fea659d3ab2853c6f9
Sergio <sergioifg94@gmail.com>
2017-03-31 18:08:58 +0200

    Documentation of Text.JSON.Control module

--- src/Shexkell/Text/JSON/Control.hs
+++ src/Shexkell/Text/JSON/Control.hs
@@ -13,17 +13,25 @@ import Data.Aeson.Types
 import qualified Data.Text as T
 
 
+-- | Parsing in the context of a JSON object
 type ObjectParser a = ReaderT Object Parser a
 
 
 parseObject :: FromJSON a => ObjectParser a -> Object -> Parser a
 parseObject = runReaderT
 
+-- | In the context of an object, obtains the value of a field by a
+--   specified key
 valueOf :: FromJSON a => T.Text -> ObjectParser a
 valueOf = getValueWith (.:)
 
+-- | In the context of an object, obtains the value of a field by a
+--   specified key. If the field is not present, returns Nothing
 valueOpt :: FromJSON a => T.Text -> ObjectParser (Maybe a)
 valueOpt = getValueWith (.:?)
 
-getValueWith :: (Object -> T.Text -> Parser a) -> T.Text -> ObjectParser a
-getValueWith f key = ask >>= \ o -> lift $ f o key
+getValueWith :: 
+     (Object -> T.Text -> Parser a) -- ^ Function that, given an object and a key, returns the value
+  -> T.Text                         -- ^ Key
+  -> ObjectParser a
+getValueWith f key = ask >>= lift . (`f` key)
